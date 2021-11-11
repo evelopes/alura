5b40ecc5ad53292ab1a15dfc3203ddee07bd8448
evelopes <evelinheks@gmail.com>
2021-11-11 20:18:28 -0300

    bootstrap

--- 07 - Bootstrap criação de uma single-page responsiva/bootstrap-boas-praticas-no-front-end-f694bc9dd5b267a71124f81eaabe68b1df3fa926/assets/js/navbar-animation-fix.js
+++ 07 - Bootstrap criação de uma single-page responsiva/bootstrap-boas-praticas-no-front-end-f694bc9dd5b267a71124f81eaabe68b1df3fa926/assets/js/navbar-animation-fix.js
@@ -1,8 +1,7 @@
-$('#collapse-navbar').on('show.bs.collapse', function() {
-	$('.topCasaFina-banner').css('transform', 'translate(-50%, 10%');
-
+$('#navbar-topCasaFina').on('show.bs.collapse', function() {
+	$('.topCasaFina-banner').css('transform', 'translate(-50%, 10%)');
 });
 
-$('#collapse-navbar').on('hide.bs.collapse', function() {
-	$('.topCasaFina-banner').css('transform', 'translate(-50%, -50%');
+$('#navbar-topCasaFina').on('hide.bs.collapse', function() {
+	$('.topCasaFina-banner').css('transform', 'translate(-50%, -50%)');
 });
