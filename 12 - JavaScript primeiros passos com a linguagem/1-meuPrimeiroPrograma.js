34597ffd305230b7ce281a454cd9f2e4cd2c82eb
evelopes <evelinheks@gmail.com>
2021-12-06 17:07:22 -0300

    js

---
+++ 12 - JavaScript primeiros passos com a linguagem/1-meuPrimeiroPrograma.js
@@ -0,0 +1,11 @@
+console.log("Olá Mundo!");
+
+const idade = 42;
+console.log(idade);
+console.log(idade+2);
+
+const idadeSomada = idade + 20
+console.log(idadeSomada)
+console.log(idade + 5)
+console.log(idade-2)
+console.log(idade/2)
---
+++ 12 - JavaScript primeiros passos com a linguagem/4-conversaoDeTipos.js
@@ -0,0 +1,11 @@
+console.log("Conversão de Tipos");
+
+console.log("ano" + 2020);
+console.log("5" + "2");
+console.log(parseInt("7") / parseInt("2"));
+
+console.log("10" / "2");
+console.log("Ricardo" / "2"); // NaN = Not a Number
+
+console.log(6.5);
+console.log(6,5);
