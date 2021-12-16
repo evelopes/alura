897bfd0103a23d8b5149aaf19576906a038ee177
evelopes <evelinheks@gmail.com>
2021-12-16 12:57:42 -0300

    js

--- 14 - JavaScript Interfaces e Herança em Orientação a Objetos/ContaCorrente.js
+++ 14 - JavaScript Interfaces e Herança em Orientação a Objetos/ContaCorrente.js
@@ -3,9 +3,8 @@ import { Conta } from "./Conta.js";
 
 export class ContaCorrente extends Conta{
     static numeroDeContas = 0;
-       // #saldo =0 https://github.com/tc39/proposal-class-fields#private-fields
-    constructor(agencia, cliente){
-         
+    constructor(cliente, agencia){
+        super(0,cliente,agencia);         
         ContaCorrente.numeroDeContas += 1;
-    }
-}
+    };
+};
