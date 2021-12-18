4d3e92b31e9a6f8c23269d92f9a52e7edfe81803
evelopes <evelinheks@gmail.com>
2021-12-18 13:15:47 -0300

    js

--- 14 - JavaScript Interfaces e Herança em Orientação a Objetos/Conta.js
+++ 14 - JavaScript Interfaces e Herança em Orientação a Objetos/Conta.js
@@ -1,32 +1,46 @@
-export class Conta {  
-  constructor(saldoInicial, cliente, agencia){
-    this._saldo = saldoInicial;
-    this._cliente = cliente;
-    this._agencia = agencia;
-}
-sacar(valor){
-    let taxa = 1
-    const valorSacado = taxa * valor;
-    if(this._saldo >= valor){
-        this._saldo -= valor;
-        return valor;
+export class Conta {
+    constructor(saldoInicial, cliente, agencia) {
+        this._saldo = saldoInicial;
+        this._cliente = cliente;
+        this._agencia = agencia;
+       
     }
-}
 
-depositar(valor){
-    if(valor <= 0)
-    {
-        return;
-    } 
-    this._saldo += valor;           
-}
+    set cliente(novoValor) {
+        if (novoValor instanceof Cliente) {
+            this._cliente = novoValor;
+        }
+    }
 
-tranferir(valor, conta){
-    
-    const valorSacado = this.sacar(valor);
-    conta.depositar(valorSacado);
-    
-}
+    get cliente() {
+        return this._cliente;
+    }
 
+    get saldo() {
+        return this._saldo;
+    }
 
+    sacar(valor) {
+        let taxa = 1
+        return this._sacar(valor, taxa);
+    }
+
+    _sacar(valor, taxa){
+        const valorSacado = taxa * valor;
+        if (this._saldo >= valorSacado) {
+            this._saldo -= valorSacado;
+            return valorSacado;
+        }
+
+        return 0;
+    }
+
+    depositar(valor) {
+        this._saldo += valor;
+    }
+
+    tranferir(valor, conta) {
+        const valorSacado = this.sacar(valor);
+        conta.depositar(valorSacado);
+    }
 }
