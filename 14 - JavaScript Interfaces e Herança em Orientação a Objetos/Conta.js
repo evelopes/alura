120188f1ceb01e4781639951e735bc68348dd60b
evelopes <evelinheks@gmail.com>
2021-12-19 12:43:38 -0300

    js

--- 14 - JavaScript Interfaces e Herança em Orientação a Objetos/Conta.js
+++ 14 - JavaScript Interfaces e Herança em Orientação a Objetos/Conta.js
@@ -1,9 +1,12 @@
 export class Conta {
     constructor(saldoInicial, cliente, agencia) {
+          if (this.constructor == Conta){
+           throw new Error("Você não deveria instanciar um objeto do tipo conta diretamente");
+       }
         this._saldo = saldoInicial;
         this._cliente = cliente;
         this._agencia = agencia;
-       
+     
     }
 
     set cliente(novoValor) {
