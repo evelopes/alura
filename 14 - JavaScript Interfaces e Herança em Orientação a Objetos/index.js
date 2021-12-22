b2c297cb4a628d3c95fd8245e83a39652a4703ab
evelopes <evelinheks@gmail.com>
2021-12-22 19:52:37 -0300

    js

--- 14 - JavaScript Interfaces e Herança em Orientação a Objetos/index.js
+++ 14 - JavaScript Interfaces e Herança em Orientação a Objetos/index.js
@@ -1,16 +1,11 @@
 import {Cliente} from "./Cliente.js";
-import {ContaCorrente} from "./Conta/ContaCorrente.js";
-import { ContaPoupanca } from "./Conta/ContaPoupanca.js";
-import { ContaSalario } from "./Conta/ContaSalario.js";
-import { Diretor } from "./Funcionarios/Diretor.js";
+import {Gerente} from "./Funcionario/Gerente.js";
+import {Diretor} from "./Funcionario/Diretor.js";
+import {sistemaAutenticacao} from "./sistemaAutenticacao.js";
 
-const cliente1 = new Cliente("Ricardo", 11122233309);
+const diretor = new Diretor("Rodrigo", 10000, 46585236589);
+const gerente = new Gerente("Ricardo", 5000,  45444236589);
 
-const contaCorrenteRicardo = new ContaCorrente( cliente1, 1001);
-const contaPoupanca = new ContaPoupanca(50, cliente1, 1001);
-const contaSalario = new ContaSalario(cliente1);
-contaSalario.depositar(100);
-contaSalario.sacar(10);
+const estaLogado = sistemaAutenticacao.login(diretor, "123456789");
 
-console.log(contaSalario);
-
+console.log(estaLogado)
