dac81a5ecb140a7e5bfc0233eb5181f81ffcbf17
evelopes <evelinheks@gmail.com>
2022-01-02 14:08:07 -0300

    js

--- 14 - JavaScript Interfaces e Herança em Orientação a Objetos/Funcionarios/Funcionario.js
+++ 14 - JavaScript Interfaces e Herança em Orientação a Objetos/Funcionarios/Funcionario.js
@@ -7,8 +7,9 @@ export class Funcionario{
     this._bonificacao = 1;
     this._senha;
   }
-  get senha(){
-    return this._senha
+
+  autenticar(senha){
+    return senha == this._senha;
   }
 
   cadastrarSenha(senha){
